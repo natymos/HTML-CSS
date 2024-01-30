f580d13ac7fcfe2cec8d1f111dfb520b13e45b8a
NatyMos <natymos@yahoo.com>
2024-01-29 23:01:33 -0500

    ejercicio2

--- javascript.js
+++ javascript.js
@@ -1,22 +1,16 @@
-const inicio = [0,1];
-let suma;
-var numeroDeElementos = parseInt(prompt("Ingrese el número de elementos de la serie fibonacci a mostrar"));
-
-if (numeroDeElementos === 0 && numeroDeElementos !== num) {
-    alert("Por favor ingrese un número válido");
-} else if (numeroDeElementos == 1) {
-    alert("la serie fibonacci es: "+0);
-} else if (numeroDeElementos == 2) {
-    alert("la serie fibonacci es: "+inicio);
-}    
-function fibonacci (numeroDeElementos){
-    const inicio=[0,1];
-     for (let i=2;i<numeroDeElementos; i++){
-        inicio.push(inicio[i-1]+ inicio[i-2]);
+const inicio=["una cadena",3,9,1,10,500,1,10,true];
+ const resultado=[];
+ for (let i=0; i<= inicio.length; i++){
+    if (typeof inicio[i]=="number"){
+        resultado.push (inicio[i]);
     }
-    return inicio;
+ }
+ console.log (resultado);
+
+ let mayor=0
+for(let i=0 ; i<resultado.length;i++){
+    if (resultado [i]>0){ 
+        mayor =resultado[i]
+    }
 }
-const serie = fibonacci (numeroDeElementos);
-alert ("la serie fibonacci es: "+serie);
-
-
+console.log (mayor);
